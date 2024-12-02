76124225786fda11bf7ca46e8910641dde19c7a8
realwebsdigital@gmail.com <realwebsdigital@gmail.com>
2024-12-02 10:53:34 +0200

    fetching image

--- api/index.ts
+++ api/index.ts
@@ -1,33 +1,19 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-// import sharp from "sharp";
 
-// export const config = {
-//   runtime: "edge",
-// };
-
-export const runtime = "nodejs";
-
-// export const pictureFetcher = async () => {
-//   const picture = await fetch("https://picsum.photos/200/300");
-//   // console.log(picture);
-
-//   return picture;
-// };
-
-// export const fun = async () => {
-//   const img = await (await pictureFetcher()).arrayBuffer();
-
-//   const imgBuffer = sharp(img).rotate(40).toBuffer();
-
-//   return await imgBuffer;
-// };
+export const config = {
+  runtime: "edge",
+};
 
 const app = new Hono().basePath("/api");
 
 app.get("/", async (c) => {
-  console.log("dasdasdasdasdasdasd");
-  return c.json({ hello: "world" });
+  // random image from
+  const picsum = "https://picsum.photos/200/300";
+
+  const imageResource = (await fetch(picsum)).body;
+
+  return c.newResponse(imageResource);
 });
 
 export default handle(app);
