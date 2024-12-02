e85396d93d9bdfb9ff4ce2765484c9b2cbb8f61d
realwebsdigital@gmail.com <realwebsdigital@gmail.com>
2024-12-02 13:24:22 +0200

    3rd vercel serveless test

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,29 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import sharp from "sharp";
 
 export const runtime = "nodejs";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello from Hono!",
-  });
+const pictureFetcher = async () => {
+  const picture = await fetch("https://picsum.photos/200/300");
+  // console.log(picture);
+
+  return picture;
+};
+
+export const fun = async () => {
+  const img = await (await pictureFetcher()).arrayBuffer();
+
+  const imgBuffer = sharp(img).rotate(40).toBuffer();
+
+  //testing 1 2
+  return await imgBuffer;
+};
+
+app.get("/hello", async (c) => {
+  return c.newResponse(await fun());
 });
 
 export const GET = handle(app);
