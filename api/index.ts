bfbc73348ac207fbebc8872ba6b117c8afa5cf1b
realwebsdigital@gmail.com <realwebsdigital@gmail.com>
2024-12-02 13:08:08 +0200

    edge config test

--- api/index.ts
+++ api/index.ts
@@ -4,18 +4,24 @@ import { handle } from "hono/vercel";
 export const config = {
   runtime: "edge",
 };
+import { get } from "@vercel/edge-config";
 
 // export const runtime = "nodejs";
 const app = new Hono().basePath("/api");
 
 app.get("/", async (c) => {
-  // random image from
-  const picsum =
-    "https://fastly.picsum.photos/id/548/200/300.jpg?hmac=dXVAc-s_U8QgoYUrMld43VmrOby1cluk-akWgxY6b9Y";
+  const x = await get("greeting");
 
-  const imageResource = (await fetch(picsum)).body;
+  console.log(x);
+  // // random image from
+  // const picsum =
+  //   "https://fastly.picsum.photos/id/548/200/300.jpg?hmac=dXVAc-s_U8QgoYUrMld43VmrOby1cluk-akWgxY6b9Y";
 
-  return c.newResponse(imageResource);
+  // const imageResource = (await fetch(picsum)).body;
+
+  return c.json({
+    message: x,
+  });
 });
 
 export default handle(app);
